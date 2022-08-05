1de420298c76e25507e8676db552c033a5aba501
andrea-agosta-salt <andrea.agosta@appliedtechnology.se>
2022-08-05 09:50:44 +0200

    02-consuming-promises complete exercise 1

--- 02-consuming-promises/answers.js
+++ 02-consuming-promises/answers.js
@@ -1,23 +1,28 @@
+const { promises } = require('graceful-fs');
+
 /**
- * 
+ *
  * EXERCISE 1
- * 
+ *
  * @param {Promise} promise
  * @param {thunk} action
- * 
+ *
  */
-function waitForPromise(promise, action){
+function waitForPromise(promise, action) {
   /* IMPLEMENT ME */
+  return new Promise((resolve) => {
+    promise.then(() => resolve(action()));
+  });
 }
 /**
- * 
+ *
  * EXERCISE 2
- * 
- * @param {Promise} promise 
- * @param {consumer} consumer 
- * @param {handler} handler 
+ *
+ * @param {Promise} promise
+ * @param {consumer} consumer
+ * @param {handler} handler
  */
-function consumePromise(promise, consumer, handler){
+function consumePromise(promise, consumer, handler) {
   /* IMPLEMENT ME! */
 }
 
